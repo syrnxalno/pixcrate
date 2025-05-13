75e21131638ae74e8f0eb06cc078658ca041d1bf
syrnxalno <archanakrishna0023@gmail.com>
2025-05-13 21:17:33 +0530

    feat: exports added

--- src/backend/workers/compressWorker.js
+++ src/backend/workers/compressWorker.js
@@ -2,21 +2,24 @@ import { Worker } from "bullmq";
 import redisConnect from "../config/redisConfig.js";
 import compressImg from "../utils/compressImage.js";
 
-try{
-const compressWorker = new Worker("compressQueue", async (job) => {
-    console.log(`Compress worker processing job with name: ${job.name}`);
-    return await compressImg(job);
-}
-, { connection: redisConnect });
+let compressWorker;
 
-compressWorker.on("completed", (job) => {
-    console.log(`Compress process ${job.id} has finished`);
-});
+try {
+    compressWorker = new Worker("compressQueue", async (job) => {
+        console.log(`Compress worker processing job with name: ${job.name}`);
+        return await compressImg(job);
+    }, { connection: redisConnect });
 
-compressWorker.on("failed", (job, err) => {
-    console.log(`Compress process ${job.id} has failed with error: ` + err);
-});
-}catch (e) {
+    compressWorker.on("completed", (job) => {
+        console.log(`Compress process ${job.id} has finished`);
+    });
+
+    compressWorker.on("failed", (job, err) => {
+        console.log(`Compress process ${job.id} has failed with error: ` + err);
+    });
+} catch (e) {
     console.error("Error initializing compressWorker:", e);
-    throw e; 
+    throw e;
 }
+
+export { compressWorker };
--- src/backend/workers/watermarkWorker.js
+++ src/backend/workers/watermarkWorker.js
@@ -2,20 +2,24 @@ import { Worker } from "bullmq";
 import redisConnect from "../config/redisConfig.js";
 import watermarkImage from "../utils/watermarkImage.js";
 
-try{
-const watermarkWorker = new Worker("watermarkQueue", async (job) => {
-    console.log(`Watermark worker processing job with name: ${job.name}`);
-    return await watermarkImage(job);
-}, { connection: redisConnect });
+let watermarkWorker;
 
-watermarkWorker.on("completed", (job) => {
-    console.log(`Watermark process ${job.id} has finished`);
-}); 
+try {
+    watermarkWorker = new Worker("watermarkQueue", async (job) => {
+        console.log(`Watermark worker processing job with name: ${job.name}`);
+        return await watermarkImage(job);
+    }, { connection: redisConnect });
 
-watermarkWorker.on("failed", (job, err) => {
-    console.log(`Watermark process ${job.id} has failed with error: ` + err);
-});
-}catch (e) {
+    watermarkWorker.on("completed", (job) => {
+        console.log(`Watermark process ${job.id} has finished`);
+    });
+
+    watermarkWorker.on("failed", (job, err) => {
+        console.log(`Watermark process ${job.id} has failed with error: ` + err);
+    });
+} catch (e) {
     console.error("Error initializing watermarkWorker:", e);
-    throw e; 
+    throw e;
 }
+
+export { watermarkWorker };
