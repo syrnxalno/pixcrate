ce3041ae9ad71cc60a4b19a431aa7ebcaeaac27c
syrnxalno <archanakrishna0023@gmail.com>
2025-05-13 21:17:57 +0530

    refactor: minor refactor

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,22 +2,24 @@ import express from 'express';
 import dotenv from 'dotenv';
 import sessionMiddleware from './middlewares/sessionMiddleware.js';
 import passport from 'passport';
-import './config/passportConfig.js';
+// import './config/passportConfig.js';
 import cors from 'cors';
 
 import imageRoutes from './routes/imageRoutes.js';
-import authRoutes from './routes/oauthRoutes.js';
+// import authRoutes from './routes/oauthRoutes.js';
 import bullboardRoutes from './routes/bullboardRoutes.js';
 
-import './workers/resizeWorker.js';
-import './workers/compressWorker.js';
-import './workers/watermarkWorker.js';
-import './workers/saveimageWorker.js';
+import { resizeWorker } from './workers/resizeWorker.js';
+import { compressWorker } from './workers/compressWorker.js';
+import { watermarkWorker } from './workers/watermarkWorker.js';
+import { saveImageWorker } from './workers/saveimageWorker.js';
+import { markWorkersAsReady,initializeFlowProducer} from './controllers/imageController.js';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
 
 app.use(passport.initialize());
 sessionMiddleware(app);
@@ -30,13 +32,32 @@ app.use(
   })
 );
 
-
-// Routes
+// Routes (attached later)
 app.use('/', imageRoutes);
-app.use('/', authRoutes);
-app.use('/', bullboardRoutes); // Bull Board UI
+// app.use('/', authRoutes);
+app.use('/', bullboardRoutes);
 
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    await Promise.all([
+      resizeWorker.waitUntilReady(),
+      compressWorker.waitUntilReady(),
+      watermarkWorker.waitUntilReady(),
+      saveImageWorker.waitUntilReady()
+    ]);
+
+    console.log('All workers are ready.');
+
+    initializeFlowProducer();  
+    markWorkersAsReady();      
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error('Worker initialization failed:', err);
+    process.exit(1); // Fail fast
+  }
+};
+
+startServer();
